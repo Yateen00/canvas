7f80a9a96f5af8b51bcb3c6e999b712a0124949d
Yateen00 <codingdudelolhow@gmail.com>
2024-06-15 20:54:40 +0530

    added getsize function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function createCanvas() {
+//elements: column, row_number
+//rows: row, id: row_number
+function createCanvas(square_side = 16) {
   let canvas = document.querySelector("#canvas");
-  let square_side = 16;
   for (let i = 0; i < square_side; i++) {
     let row = document.createElement("div");
     row.setAttribute("id", `${i + 1}`);
@@ -9,10 +10,28 @@ function createCanvas() {
     for (let j = 0; j < square_side; j++) {
       let column = document.createElement("div");
       column.classList.add(`${i + 1}`, "column");
-
       row.appendChild(column);
     }
     canvas.appendChild(row);
   }
 }
-createCanvas();
+function changeColor(event) {
+  let element = event.target;
+  element.style.backgroundColor = color;
+}
+function getSize() {
+  let size = prompt("Enter the size of the canvas(max 100)");
+  size = parseInt(size);
+  if (size > 100) {
+    size = 100;
+  }
+  if (Number.isNaN(size)) {
+    size = 16;
+  }
+  return size;
+}
+let color = "grey";
+size = getSize();
+createCanvas(size);
+let canvas = document.querySelector("#canvas");
+canvas.addEventListener("mouseover", changeColor);
