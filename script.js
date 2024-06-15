b418f88cb67fbfb9278fb60daeb2f4a88691d0d0
Yateen00 <codingdudelolhow@gmail.com>
2024-06-15 21:36:43 +0530

    added utility functionality

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //elements: column, row_number
 //rows: row, id: row_number
 function createCanvas(square_side = 16) {
+  clearCanvas();
   let canvas = document.querySelector("#canvas");
+  canvas.addEventListener("mouseover", changeColor);
   for (let i = 0; i < square_side; i++) {
     let row = document.createElement("div");
     row.setAttribute("id", `${i + 1}`);
@@ -17,10 +19,12 @@ function createCanvas(square_side = 16) {
 }
 function changeColor(event) {
   let element = event.target;
-  element.style.backgroundColor = color;
+  if (element.classList.contains("column")) {
+    element.classList.add("colored");
+    element.style.backgroundColor = color;
+  }
 }
-function getSize() {
-  let size = 16;
+function parseSize(size = 16) {
   size = parseInt(size);
   if (size > 100) {
     size = 100;
@@ -30,8 +34,26 @@ function getSize() {
   }
   return size;
 }
+function clearCanvas() {
+  let coloredColumns = document.querySelectorAll(".colored");
+  input.value = "";
+  coloredColumns.forEach((column) => {
+    column.classList.remove("colored");
+    column.style.backgroundColor = "white";
+  });
+}
+function changeSize() {
+  let size = parseSize(input.value);
+  createCanvas(size);
+}
+
 let color = "grey";
-size = getSize();
-createCanvas(size);
-let canvas = document.querySelector("#canvas");
-canvas.addEventListener("mouseover", changeColor);
+let resize = document.querySelector("#resize");
+let clear = document.querySelector("#clear");
+let input = document.querySelector("input");
+
+resize.addEventListener("click", changeSize);
+input.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") changeSize();
+});
+clear.addEventListener("click", clearCanvas);
