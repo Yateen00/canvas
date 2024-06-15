d22809b543ec3d4b635e03dfb3d00a58f72d903e
Yateen00 <codingdudelolhow@gmail.com>
2024-06-15 22:52:16 +0530

    added functionality for random color and opacity

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //elements: column, row_number
 //rows: row, id: row_number
+
+//canvas related functions
+let defaultBgColor = "white";
+let defaultOpacity = "1";
 function createCanvas(square_side = 16) {
   clearCanvas();
   let canvas = document.querySelector("#canvas");
-  canvas.addEventListener("mouseover", changeColor);
   for (let i = 0; i < square_side; i++) {
     let row = document.createElement("div");
     row.setAttribute("id", `${i + 1}`);
@@ -12,18 +15,42 @@ function createCanvas(square_side = 16) {
     for (let j = 0; j < square_side; j++) {
       let column = document.createElement("div");
       column.classList.add(`${i + 1}`, "column");
+      column.setAttribute(
+        "style",
+        `background-color: ${defaultBgColor}; opacity:${defaultOpacity};`
+      );
       row.appendChild(column);
     }
     canvas.appendChild(row);
   }
+  canvas.addEventListener("mouseover", changeColor);
+  canvas.addEventListener("click", changeColor);
 }
 function changeColor(event) {
   let element = event.target;
   if (element.classList.contains("column")) {
-    element.classList.add("colored");
-    element.style.backgroundColor = color;
+    let bg = element.style.backgroundColor;
+    let opacity = element.style.opacity;
+    opacity = parseFloat(opacity);
+    if (bg === defaultBgColor && opacity.toString() === defaultOpacity) {
+      element.classList.add("colored");
+      element.style.backgroundColor = getRandomColor();
+      element.style.opacity = "0.1";
+    } else if (opacity < 1) {
+      opacity = (opacity * 10 + 1) / 10;
+      element.style.opacity = opacity.toString();
+    }
+  }
+  function getRandomColor() {
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += Math.floor(Math.random() * 16).toString(16);
+    }
+    return color;
   }
 }
+
+//utility functions
 function parseSize(size = 16) {
   size = parseInt(size);
   if (size > 100) {
@@ -46,8 +73,6 @@ function changeSize() {
   let size = parseSize(input.value);
   createCanvas(size);
 }
-
-let color = "grey";
 let resize = document.querySelector("#resize");
 let clear = document.querySelector("#clear");
 let input = document.querySelector("input");
@@ -57,3 +82,6 @@ input.addEventListener("keyup", (e) => {
   if (e.key === "Enter") changeSize();
 });
 clear.addEventListener("click", clearCanvas);
+alert(
+  "input a size, and click or hover mouse to draw. random colour each time."
+);
